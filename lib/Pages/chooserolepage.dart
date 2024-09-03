872b169e321b5ac1a7af78404544dd0f9041541a
AqilHanan <148228299+AqilHanan@users.noreply.github.com>
2024-09-03 10:12:12 +0800

    Revert "Revert "version 0.645""
    This reverts commit 80dce0a0

--- lib/Pages/chooserolepage.dart
+++ lib/Pages/chooserolepage.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 import 'delivererpage.dart';
-import 'giverpage.dart';
 import 'takerpage.dart';
 import 'testgiverpage.dart';
 
@@ -16,6 +18,8 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Background remains the same color
+      backgroundColor: Colors.greenAccent[200],
       body: SingleChildScrollView(
         child: SafeArea(
           child: Center(
@@ -28,11 +32,25 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
                   height: 100,
                   width: 200,
                   child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen[200], // Set button color to white
+                      side: BorderSide(color: Colors.amber[100]!, width: 10), // Amber outer ring
+                      shadowColor: Colors.black,
+                      elevation: 6,
+                    ),
                     onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => TestGiverPage()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TestGiverPage()));
                     },
-                    child: Text("Giver", style: TextStyle(fontSize: 24)),
+                    child: Row(
+                      children: [
+                        Text("Giver", style: TextStyle(fontSize: 24,color: Colors.black)),
+                        SizedBox(width: 50),
+                        Icon(CupertinoIcons.heart_circle,size: 35),
+                      ],
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -42,6 +60,12 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
                   height: 100,
                   width: 200,
                   child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen[200], // Set button color to white
+                      side: BorderSide(color: Colors.amber[100]!, width: 10), // Amber outer ring
+                      shadowColor: Colors.black,
+                      elevation: 6,
+                    ),
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -52,7 +76,13 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
                         ),
                       );
                     },
-                    child: Text("Taker", style: TextStyle(fontSize: 24)),
+                    child: Row(
+                      children: [
+                        Text("Taker", style: TextStyle(fontSize: 24,color: Colors.black)),
+                        SizedBox(width: 45),
+                        Icon(Icons.handshake_outlined,size: 35,),
+                      ],
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -62,6 +92,12 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
                   height: 100,
                   width: 200,
                   child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightGreen[200], // Set button color to white
+                      side: BorderSide(color: Colors.amber[100]!, width: 10), // Amber outer ring
+                      shadowColor: Colors.black,
+                      elevation: 6,
+                    ),
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -70,7 +106,13 @@ class _ChooseRolePageState extends State<ChooseRolePage> {
                         ),
                       );
                     },
-                    child: Text("Delivery", style: TextStyle(fontSize: 24)),
+                    child: Row(
+                      children: [
+                        Text("Delivery", style: TextStyle(fontSize: 24,color: Colors.black)),
+                        SizedBox(width: 25),
+                        Icon(Icons.motorcycle_sharp,size: 35),
+                      ],
+                    ),
                   ),
                 ),
               ],
